885b89cbda715f575c09134e447020dc6a0ef760
GSteffun <42888439+GSteffun@users.noreply.github.com>
2021-10-29 15:05:00 +0300

    Закончил задачу 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = applicationContext.getBean(HelloWorld.class);
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
         System.out.println(bean.getMessage());
+        System.out.println("bean == bean1: " + (bean == bean1));
+        System.out.println("cat 1 == cat 2: " + (cat1 == cat2));
+        System.out.println("cat 2 == cat 1: " + (cat2 == cat1));
     }
 }
